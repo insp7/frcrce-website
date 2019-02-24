fcea2cada4908ea0dfc5594d6ed73ffc75d8b530
Dhano <dhananjay62.dg@gmail.com>
2019-02-24 15:45:16 +0530

    Removed unnecessary comments from footer.php due to which there was a space between footer and content

--- includes/ui-elements/footer.php
+++ includes/ui-elements/footer.php
@@ -6,17 +6,6 @@
  * Time: 11:52 PM
  */
 ?>
-
-<!-- CUSTOM FOOTER -->
-<!--<footer class="main-footer">-->
-<!--    <!-- To the right -->-->
-<!--    <div class="pull-right hidden-xs">-->
-<!--        Anything you want-->
-<!--    </div>-->
-<!--    <!-- Default to the left -->-->
-<!--    <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.-->
-<!--</footer>-->
-
 <!-- Main Footer of the theme owner -->
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
