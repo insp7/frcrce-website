5b5dc862774bc59bb24b3165607d21e880b5bd3f
Dhano <dhananjay62.dg@gmail.com>
2019-03-29 23:26:32 +0530

    added parsley js

---
+++ assets/pages/teacher/fill-remaning-details.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $('#remaining-details-form').parsley().on('field:validated', function() {
+        var ok = $('.parsley-error').length === 0;
+        $('.bs-callout-info').toggleClass('hidden', !ok);
+        $('.bs-callout-warning').toggleClass('hidden', ok);
+    })
+        .on('form:submit', function() {
+            return true; // Don't submit form for this demo
+        });
+});
--- classes/GeneralFunctions.php
+++ classes/GeneralFunctions.php
@@ -268,6 +268,48 @@
 
         public static function generalInsert($relation_name, $attributes) {
 
+            global $database;
+
+            $conn = $database->getConnection();
+
+            $columnString = implode(", ", array_keys($attributes));
+            $valueString = ":".implode(", :", array_keys($attributes));
+
+            $sql = "INSERT INTO {$relation_name} ({$columnString}) VALUES ({$valueString})";
+
+            $ps = $conn->prepare($sql);
+
+            $result = $ps->execute($attributes);
+            if($result){
+                return $conn->lastInsertId();
+            }else{
+                return false;
+            }
+
         }
+
+        function generalUpdate($table, $data, $condition){
+            global $database;
+
+            $conn = $database->getConnection();
+
+            $i = 0;
+            $columnValueSet = "";
+            foreach ($data as $key=>$value){
+                $comma = ($i<count($data)-1?", ":"");
+                $columnValueSet .= $key. "='".$value."'".$comma;
+                $i++;
+            }
+            $sql = "UPDATE $table SET $columnValueSet WHERE $condition";
+            $ps = $conn->prepare($sql);
+
+            $result = $ps->execute();
+            if($result){
+                return $ps->rowCount();
+            }else{
+                return false;
+            }
+        }
+
     }
 ?>
